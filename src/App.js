6946cf34694c9096a7f9ee3eed5ccb1c730f5100
Shubham Singh <shubhamsingh.cse20@jecrc.ac.in>
2022-12-03 13:56:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Temporary from './components/Temporary';
+
+function App() {
+  return (
+    <div className="App">
+      <Temporary/>
+    </div>
+  );
+}
+
+export default App;
